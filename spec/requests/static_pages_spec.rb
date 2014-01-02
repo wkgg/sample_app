de79eea99e5c82cbb2a2c4aa4ddefb5aa243d1ad
Amoscoder <wky1991@gmail.com>
2014-01-03 00:02:15 +0800

    finish static-pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+    	visit '/static_pages/home'
+    	page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+    	visit '/static_pages/home'
+    	page.should have_selector('title',
+    						:text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the h1 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_selector('h1', :text => 'Help')
+  	end
+
+  	it "should have the right title" do
+  		visit '/static_pages/help'
+  		page.should have_selector('title', 
+  							:text => 'Ruby on Rails Tutorial Sample App | Help')
+  	end
+  end
+
+  describe "about page" do
+
+  	it "should have the h1 'about us'" do
+  		visit '/static_pages/about'
+  		page.should have_selector('h1', :text => 'about us')
+  	end
+
+  	it "should the right title" do
+  		visit '/static_pages/about'
+  		page.should have_selector('title', 
+  							:text => 'Ruby on Rails Tutorial Sample App | About')
+  	end
+  end
+
+end
+
+
+
+
+
+
+
